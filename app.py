42ffd20d2b54022ba53f25d935cbd83eae9c78a1
elizabeth <elizabeth.olshanetsky@yale.edu>
2023-11-26 16:28:07 -0500

    added filtering and in progress changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ setattr(routing, 'basestring', str)
 # app.config['SESSION_FILE_DIR'] = './.flask_session/'
 # Session(app)
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 cas = CAS(app)
 app.config['CAS_SERVER'] = 'https://secure6.its.yale.edu/cas/'
 app.config['CAS_AFTER_LOGIN'] = 'https://127.0.0.1:5001/'
@@ -31,20 +31,6 @@ app.secret_key = os.urandom(64)
 
 #-----------------------------------------------------------------
 
-#how do we add the projects data here from DATABASE
-
-projects = [
-    {
-    'id': 1,
-    'project_title': 'Hi there',
-    'project_description': 'I am project',
-    'author': 'Rosa Chang',
-    }
-
-]
-
-
-
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 @login_required
@@ -59,30 +45,86 @@ def index():
 @app.route('/projects')
 def project_nav():
 
-    query = "SELECT id, project_title, student_name FROM student_projects"
+    query = "SELECT id, project_title, student_name, classyear, track, thumbnail_alt FROM student_projects"
 
-    with sqlite3.connect('student_projects.db') as connection:
+    with sqlite3.connect('projects.db') as connection:
         cursor = connection.cursor()
         cursor.execute(query)
-        projects = cursor.fetchall()
+        projects_data = cursor.fetchall()
 
-    for project in projects:
-        #maybe buikd paths here to pass to template
-        project['thumbnail_path'] = f'./static/projects/{project["id"]}/images/thumbnail.png'
-        # project_title_path = project['project_title']
-        # student_name_path = project['student_path']
-        # try:
-        #     with open(project_title_path, 'r') as title_file:
-        #         project['project_title'] = title_file.read()
-        # except FileNotFoundError:
-        #     project['project_title'] = 'Title not found'
-        # try:
-        #     with open(student_name_path, 'r') as name_file:
-        #         project['student_path'] = name_file.read()
-        # except FileNotFoundError:
-        #     project['student_path'] = 'Name not found'
-    return render_template('project_nav.html', projects=projects)
+    projects = []
+    for project_data in projects_data:
+        project = {
+            'id': project_data[0],
+            'url': f"/project/{project_data[0]}",
+            'thumbnail_path': f'./static/projects/{project_data[0]}/images/thumbnail.png',
+            'project_title': project_data[1],
+            'student_name': project_data[2],
+            'classyear': project_data[3],
+            'track': project_data[4],
+            'thumbnail_alt': project_data[5],
+            }
+    projects.append(project)
+       
+    yearquery = "SELECT DISTINCT classyear FROM student_projects"
+    
+    with sqlite3.connect('projects.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute(yearquery)
+        classyears_data = cursor.fetchall()
 
+    # Extract years from the result
+        classyear_options = [years[0] for years in classyears_data]
+
+    # Query for distinct tracks
+    track_query = "SELECT DISTINCT track FROM student_projects"
+    with sqlite3.connect('projects.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute(track_query)
+        tracks_data = cursor.fetchall()
+        track_options = [track[0] for track in tracks_data]
+
+    return render_template('project_nav.html', projects=projects, classyear_options=classyear_options, track_options=track_options)
+
+
+#Render each individual project when the project is selected
+@app.route('/project/<int:project_id>')
+def project_details(project_id):
+
+    #Get all the data we need for project details
+    with sqlite3.connect('projects.db') as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM student_projects WHERE id = ?", (project_id,))
+        project_data = cursor.fetchone()
+
+    if project_data:
+        # Dictionary with info about project to use in template
+        project = {
+            'project_title': project_data['project_title'],
+            'student_name': project_data['student_name'],
+            'project_description': project_data['project_description'],
+            'thumbnail_path': f'./static/projects/{project_data[0]}/images/thumbnail.png',
+            'thumbnail_alt': project_data['thumbnail_alt'],
+            'track': project_data['track'],
+            'classyear': project_data['classyear'],
+            'image1_path': f'./static/projects/{project_data[0]}/images/image1.png',
+            'image1_alt': project_data['image1_alt'],
+            'image2_alt': project_data['image2_alt'],
+            'video1_link': project_data['video1_link'],
+            'video1_alt': project_data['video1_alt'],
+            'video2_link': project_data['video2_link'],
+            'video2_alt': project_data['video2_alt'],
+        }
+
+        # Renders project details page
+        return render_template('project_details.html', project=project)
+    else:
+        # Project with ID is not found
+        return 'Project not found', 404
+
+
+#Route for adding new projects
 @app.route("/newproject", methods=["GET", "POST"])
 @login_required
 def newproject():
